b67cb05787b9e477a1ae225a0978d9b6139c67aa
Federico Capoccia <rich.book7416@fastmail.com>
2026-05-23 19:48:33 +0200

    I don't really feel like writing a commit

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -2,28 +2,7 @@
 {
   programs.ssh = {
     enable = true;
-    package = null;
-
     enableDefaultConfig = false;
-    matchBlocks."*" = {
-      forwardAgent = false;
-      addKeysToAgent = "yes";
-      compression = false;
-      serverAliveInterval = 0;
-      serverAliveCountMax = 3;
-      hashKnownHosts = false;
-      userKnownHostsFile = "~/.ssh/known_hosts";
-      controlMaster = "no";
-      controlPath = "~/.ssh/master-%r@%n:%p";
-      controlPersist = "no";
-    };
-
-    matchBlocks."codeberg.org" = {
-      hostname = "codeberg.org";
-      user = "git";
-      identityFile = "~/.ssh/git_ed25519";
-      identitiesOnly = true;
-    };
   };
 
   home.sessionVariables = {
