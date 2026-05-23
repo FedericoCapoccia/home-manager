588a80b4c14ff4e6e9f064816c78ee556e7f2259
Federico Capoccia <rich.book7416@fastmail.com>
2026-05-23 19:55:23 +0200

    I don't really feel like writing a commit

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -5,6 +5,11 @@
     enableDefaultConfig = false;
   };
 
+  settings."codeberg.org" = {
+    IdentityFile = "~/.ssh/git_ed25519";
+    IdentitiesOnly = true;
+  };
+
   home.sessionVariables = {
     SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/gcr/ssh";
   };
