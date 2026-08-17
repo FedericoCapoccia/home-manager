7cdf5278fc54186703cee48cba37dececd97db5b
Federico Capoccia <rich.book7416@fastmail.com>
2026-08-17 15:29:12 +0200

    Fix all errors, all errors on the WORLD!!!!

--- home-manager/modules/opencode.nix
+++ home-manager/modules/opencode.nix
@@ -1,4 +1,9 @@
-{ pkgs, config, lib, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 let
   opencodeWrapper = pkgs.writeShellScriptBin "opencode" ''
     exec "${config.home.homeDirectory}/.opencode/bin/opencode" "$@"
@@ -10,7 +15,7 @@ in
     package = opencodeWrapper;
 
     settings = {
-      model = "opencode/qwen3.6-plus";
+      model = "ollama/metalspork/qwen3.8-ud:Q2_K_XL";
       provider = {
         github-copilot = {
           whitelist = [
@@ -24,6 +29,18 @@ in
             "gpt-5.4-mini"
             "gpt-5.5"
           ];
+        };
+        ollama = {
+          npm = "@ai-sdk/openai-compatible";
+          name = "Ollama";
+          options = {
+            baseURL = "http://localhost:11434/v1";
+          };
+          models = {
+            "metalspork/qwen3.8-ud:Q2_K_XL" = {
+              name = "Qwen 3.8 UD Q2_K_XL";
+            };
+          };
         };
       };
       mcp = { };
